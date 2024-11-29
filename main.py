3d395f1f7a08b648a3ac38bac1567f0d037ca329
JunW-Git <jia.jun.eight@gmail.com>
2024-11-29 15:46:00 +1100

    Add main.py

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ import pyglet
 
 width, height = 1000, 1000
 window = pyglet.window.Window(width, height)
+fullscreen = True
+window.set_fullscreen(fullscreen=fullscreen)
+
+#--------------------------------------------------------------------------------------------------------------
+
+@window.event
+def on_key_press(symbol, modifiers):
+    pass
+
+@window.event
+def on_key_release(symbol, modifiers):
+    global fullscreen
+    if symbol == pyglet.window.key.F:
+        if fullscreen == True:
+            fullscreen = False
+            window.set_fullscreen(fullscreen=fullscreen)
+        else:
+            fullscreen = True
+            window.set_fullscreen(fullscreen=fullscreen)
+
+#--------------------------------------------------------------------------------------------------------------
 
 pyglet.app.run()

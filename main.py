0d1745711f977e5d14aa2ab90da006e36a830016
JunW-Git <jia.jun.eight@gmail.com>
2024-11-29 16:40:32 +1100

    there's a square!!

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import random
 import numpy
 import pyglet
 
-width, height = 1000, 1000
-window = pyglet.window.Window(width, height)
-fullscreen = True
-window.set_fullscreen(fullscreen=fullscreen)
+display = pyglet.canvas.get_display()
+screen = display.get_screens()[-1] # Get the bigger screen (I use a laptop with a moniter)
+window = pyglet.window.Window(fullscreen=True, caption="Dot Life", vsync=False, screen=screen)
+
+time = 0.01 # Time between each increment (allows for slow mo and time elapse)
+
+SCREEN_WIDTH, SCREEN_HEIGHT = window.get_size()[0], window.get_size()[1]
+
+WIDTH, HEIGHT = 1000, 1000
+CENTER_X, CENTER_Y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+
+BORDER = pyglet.graphics.Batch()
+UP_BORDER = pyglet.shapes.Rectangle(x=CENTER_X-501, y=CENTER_Y+501, width=1002, height=1, color=(255, 255, 255), batch=BORDER)
+BOTTOM_BORDER = pyglet.shapes.Rectangle(x=CENTER_X-501, y=CENTER_Y-501, width=1002, height=1, color=(255, 255, 255), batch=BORDER)
+LEFT_BORDER = pyglet.shapes.Rectangle(x=CENTER_X-501, y=CENTER_Y-501, width=1, height=1002, color=(255, 255, 255), batch=BORDER)
+RIGHT_BORDER = pyglet.shapes.Rectangle(x=CENTER_X+501, y=CENTER_Y-501, width=1, height=1002, color=(255, 255, 255), batch=BORDER)
 
 #--------------------------------------------------------------------------------------------------------------
 
@@ -15,15 +27,14 @@ def on_key_press(symbol, modifiers):
 
 @window.event
 def on_key_release(symbol, modifiers):
-    global fullscreen
-    if symbol == pyglet.window.key.F:
-        if fullscreen == True:
-            fullscreen = False
-            window.set_fullscreen(fullscreen=fullscreen)
-        else:
-            fullscreen = True
-            window.set_fullscreen(fullscreen=fullscreen)
+    pass
 
 #--------------------------------------------------------------------------------------------------------------
 
+def update(dt):
+    window.clear()
+
+    BORDER.draw()
+
+pyglet.clock.schedule_interval(update, time)
 pyglet.app.run()
